fca06f82d00c9e9f822f4adf9096d1b05af9864d
jullianrives7 <114496707+jullianrives7@users.noreply.github.com>
2022-11-15 11:41:07 -0600

    updated api-server config.js connectionstring.1

--- api-server/config.js
+++ api-server/config.js
@@ -2,7 +2,7 @@ module.exports = {
   dev: {
     // connectionString: "postgresql://postgres:docker@127.0.0.1:5432/mvp_db",
     connectionString:
-      "postgres://mvp_db_k3il_user:Ol3hwGo72Nb9kf4K9rJn3FPlYnBxYa4n@dpg-cdpsf71gp3jr9p60aeug-a.ohio-postgres.render.com/mvp_db_k3il",
+      "postgres://mvp_db_k3il_user:Ol3hwGo72Nb9kf4K9rJn3FPlYnBxYa4n@dpg-cdpsf71gp3jr9p60aeug-a/mvp_db_k3il",
     port: "3001",
   },
   production: {
